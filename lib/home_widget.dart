5c5e77fdaf73f8acbce378c4b5affbbe750e561f
kashishmadan <40457149+kashishmadan@users.noreply.github.com>
2020-04-19 18:32:58 +0530

    Navigation Bar Added

--- lib/home_widget.dart
+++ lib/home_widget.dart
@@ -7,29 +7,38 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
- @override
+  int _currentIndex = 0;
+  final List<Widget> _children = [];
+  @override
  Widget build(BuildContext context) {
    return Scaffold(
      appBar: AppBar(
        title: Text('My Flutter App'),
      ),
+     body: _children[_currentIndex], // new
      bottomNavigationBar: BottomNavigationBar(
-       currentIndex: 0, // this will be set when a new tab is tapped
+       onTap: onTabTapped, // new
+       currentIndex: _currentIndex, // new
        items: [
-         BottomNavigationBarItem(
-           icon: new Icon(Icons.home),
-           title: new Text('Home'),
+         new BottomNavigationBarItem(
+           icon: Icon(Icons.home),
+           title: Text('Home'),
          ),
-         BottomNavigationBarItem(
-           icon: new Icon(Icons.mail),
-           title: new Text('Messages'),
+         new BottomNavigationBarItem(
+           icon: Icon(Icons.mail),
+           title: Text('Messages'),
          ),
-         BottomNavigationBarItem(
+         new BottomNavigationBarItem(
            icon: Icon(Icons.person),
            title: Text('Profile')
          )
        ],
      ),
    );
+ }
+ void onTabTapped(int index) {
+   setState(() {
+     _currentIndex = index;
+   });
  }
 }
